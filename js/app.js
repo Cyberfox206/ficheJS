477c552c3d1b51a87f8bae486fb9aab729f945ef
Cyberfox206 <bbrontesi@edenschool.fr>
2022-09-23 14:55:19 +0200

    [200~TYPE DE VARIABLE ET OPÉRATIONS SUR LES VARIABLES

--- js/app.js
+++ js/app.js
@@ -10,3 +10,43 @@ console.log(typeof age);
 // prénom est une variable de type string
 const prenom = "brennan";
 console.log(typeof prenom);
+
+//comment faire une addition
+let addition = 4 + 12;
+console.log(addition);
+
+
+//comment faire une soustraction
+let soustraction = 2-50;
+console.log(soustraction);
+
+
+//comment faire une multiplication
+let multiplication = 3 * 8;
+console.log(multiplication);
+
+
+//comment faire une division
+let division = 7/2;
+console.log(division)
+
+
+//comment faire un modulo
+let modulo = 5%2;
+console.log(modulo)
+
+//comment faire un boolean
+let test = 3 > 5;
+console.log(test)
+console.log(typeof test)
+
+// operateur de comparaison: ==; ===; !==; !=; <; >; <=; >= ;
+
+
+//l'opération réaliser est une addition des chaine de caractère
+let identity = "bonjour" + "prenom" + "vous avez" + "age" + "ans"
+console.log(identity)
+
+let majuscule = identity.toUpperCase();
+console.log(majuscule)
+
