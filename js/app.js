11f6aade203539dd13df310d5ecec7b1061c07bc
Cyberfox206 <bbrontesi@edenschool.fr>
2022-09-23 13:56:18 +0200

    VARIABLES

--- js/app.js
+++ js/app.js
@@ -1,2 +1,12 @@
 alert("hello");
 console.log("hello");
+
+
+// age est une variable de type number ou de type int
+let age;
+age = 16;
+console.log(typeof age);
+
+// prénom est une variable de type string
+const prenom = "brennan";
+console.log(typeof prenom);
